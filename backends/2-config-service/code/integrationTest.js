81573c9de8c00daf32ee274aa5f0c46f2aa28f0a
Kinman Lam <kinmanl@amazon.com>
2023-05-15 12:56:53 -0700

    Using SSM and SSPS to store params and creds

--- backends/2-config-service/code/integrationTest.js
+++ backends/2-config-service/code/integrationTest.js
@@ -2,27 +2,31 @@ const gConfig = require('./../../gConfig.json')
 const assert = require('assert')
 const axios = require('axios')
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js')
+const AWS = require('aws-sdk')
 const {describe, before, it} = require('mocha')
 const { log } = require('console')
 
 describe('API Data Endpoint', function () {
   const rootUrl = gConfig.configApiGateway
   const poolData = {
-    UserPoolId: gConfig.cognito.userPoolId,
-    ClientId: gConfig.cognito.clientId
-  };
-  
+    UserPoolId: null,
+    ClientId: null
+  }
+
   const login = {
-    Username: gConfig.cognito.username,
-    Password: gConfig.cognito.password,
+    Username: null,
+    Password: null
   }
   var accessToken
+
+    this.timeout(5000)
+  AWS.config.update({region: 'us-east-1'})
   
   // Set up Mocha hooks to authenticate with Cognito and get an authorization token
   before(async () => {
+    await GetConfig()
     await GetJwtTokens()
   })
-  
   describe('PUT /store', function () {
     it('should set the store to closed and return status 200', async function () {
       let response = await SetStore('closed')
@@ -73,6 +77,35 @@ describe('API Data Endpoint', function () {
     catch (error) {
       console.log(error.message)
     }
+  }
+  async function GetConfig() {
+    const userpool_pname =  '/Serverlesspresso/core/userpool'
+    const userpoolclient_pname =  '/Serverlesspresso/core/userpoolclient'
+    const apilogin_pname =  '/Serverlesspresso/core/apilogin'
+
+    let ssm = new AWS.SSM()
+    let ssps = new AWS.SecretsManager()
+
+    // Get the UserPool Details from Parameter Store
+    let p1 = ssm.getParameter({Name: userpool_pname}).promise()     
+    let p2 = ssm.getParameter({Name: userpoolclient_pname}).promise()
+
+    await Promise.all([p1, p2]).then((data) => {
+      data.forEach(element => {
+        if (element.Parameter.Name == userpool_pname) {
+          poolData.UserPoolId = element.Parameter.Value
+        }
+        if (element.Parameter.Name == userpoolclient_pname) {
+          poolData.ClientId = element.Parameter.Value
+        }
+      })
+    })
+    
+    await ssps.getSecretValue({SecretId: apilogin_pname}).promise()
+      .then(data => {
+        login.Username = JSON.parse(data.SecretString).Username
+        login.Password = JSON.parse(data.SecretString).Password
+    })
   }
 
   async function GetJwtTokens() {
---
+++ instructions/content/4-integration-testing/4-listener-test/_index.en.md
@@ -0,0 +1,6 @@
++++
+title = "Testing Asynchronous Services"
+weight = 14
++++
+
+In this section you setup and run integration testing on the `config-service` which a Lambda function which to trigger a Amazon EventBridge event when a change has been made to `serverlesspresso-config-table`.
